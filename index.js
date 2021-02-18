a4506d91a1b82bb0133207452036ff709ae6926e
daffychuy <danielchu0710@hotmial.com>
2021-02-18 16:44:15 -0500

    Add some enmap configuration

--- index.js
+++ index.js
@@ -10,30 +10,91 @@ const {
     getMap,
 } = require('./DataParser');
 
-const GlobalPrefix = config.prefix;
 const client = new Discord.Client({
     presence: {
         activity: {
-           type: `PLAYING`,
-           name: `BeatSaber`,
+            type: 'PLAYING',
+            name: 'BeatSaber',
         },
-        status: "idle",
-        ws: { intents: ["GUILDS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"] }
-     }
+        status: 'idle'
+    }
 });
+const Enmap = require('enmap');
+// const client = new Discord.Client({
+//     presence: {
+//         activity: {
+//            type: `PLAYING`,
+//            name: `BeatSaber`,
+//         },
+//         status: "idle",
+//         ws: { intents: ["GUILDS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"] }
+//      }
+// });
+
+client.settings = new Enmap({
+    name: "settings",
+    fetchAll: false,
+    autoFetch: true,
+    cloneLevel: 'deep',
+    autoEnsure: {
+      prefix: ">",
+      modLogChannel: "mod-log",
+      modRole: "mod",
+      adminRole: "admin",
+      welcomeChannel: "welcome",
+      welcomeMessage: "Say hello to {{user}}, everyone!"
+    },
+    
+  });
 
 client.login(config.BOT_TOKEN);
 
+client.on("guildDelete", guild => {
+    // When the bot leaves or is kicked, delete settings to prevent stale entries.
+    client.settings.delete(guild.id);
+});
+
+  
+
 client.on("message", async (message) => {
     // Base case to check if request is coming from user or a bot and if its a command
-    if (message.author.bot || !message.content.startsWith(GlobalPrefix)) return;
+    if (!message.guild || message.author.bot) return;
     
-    const commandBody = message.content.slice(GlobalPrefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
+    const guildConf = client.settings.get(message.guild.id);
+
+    if(message.content.indexOf(guildConf.prefix) !== 0) return;
+
+
+    const args = message.content.split(/\s+/g);
+    const command = args.shift().slice(guildConf.prefix.length).toLowerCase();
+ 
     const userID = message.author.id;
 
-    if (command === "ping") {
+    if (command === "showconf") {
+        let configProps = Object.keys(guildConf).map(prop => {
+            return `${prop}  :  ${guildConf[prop]}`;
+        });
+        message.channel.send(`The following are the server's current configuration:
+        \`\`\`${configProps.join("\n")}\`\`\``);
+
+    } else if (command === "setprefix") {
+        const adminRole = message.guild.roles.cache.find(role => role.name === guildConf.adminRole);
+        if(!adminRole) return message.reply("Administrator Role Not Found");
+
+        if (args.length !== 1 || args[0].length !== 1) {
+            message.reply('please supply a valid prefix');
+            return
+        }
+
+        if(!message.member.roles.cache.has(adminRole.id)) {
+            return message.reply("You're not an admin, sorry!");
+        }
+
+        client.settings.set(message.guild.id, args[0], 'prefix');
+
+        message.reply(`new prefix for the server: ${args[0]}`)
+
+    } else if (command === "ping") {
         const timeTaken = Date.now() - message.createdTimestamp;
         const timeToPing = client.ws.ping;
         message.reply(`Pong! Latency: ${timeTaken}ms. API latency: ${timeToPing}`);
